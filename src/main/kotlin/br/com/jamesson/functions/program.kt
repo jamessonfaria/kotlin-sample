0c4c488ad88f9abc205a3f1cfd60d00f508ef064
Jamesson Faria <jamessonjr@gmail.com>
2022-07-13 11:29:10 -0300

    Declaring functions

---
+++ src/main/kotlin/br/com/jamesson/functions/program.kt
@@ -0,0 +1,14 @@
+package br.com.jamesson.functions
+
+fun main(args: Array<String>) {
+    display("Hello, world")
+
+    println(max(1,2))
+}
+
+fun display(message: String) : Boolean{
+    println(message)
+    return true
+}
+
+fun max(a: Int, b: Int): Int = if (a > b) a else b
