0831205526a7d468229a9e5f9554257628e36b10
Jamesson Faria <jamessonjr@gmail.com>
2022-07-13 10:13:07 -0300

    Adjust

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,11 +7,16 @@ fun main(args: Array<String>) {
     q.display()
 
     println("The answer to the question ${q.Question} is ${q.Answer}")
+
+    val message = if(q.Answer == q.CorrectAnswer) "You were correct" else "Try again?"
+
+    println(message)
 }
 
 class Question(){
     var Answer:String = ""
     val Question:String = "What is the answer to life"
+    val CorrectAnswer = "42"
 
     fun display(){
         println("You said $Answer")
