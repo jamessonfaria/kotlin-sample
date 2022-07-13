a88e5c2521f29883d34eb948e4022af5918f8fb8
Jamesson Faria <jamessonjr@gmail.com>
2022-07-13 11:13:30 -0300

    Using looping constructs, ranges

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,56 @@
 import java.lang.NumberFormatException
+import java.util.*
 
 fun main(args: Array<String>) {
-    println("Hello, world")
-    val q:Question = Question()
-    //q.Answer  = "42"
-    q.Answer  = "Four"
-   //.Question = ""
 
+    var numbers = listOf(1,2,3,4,5)
 
-    q.display()
-
-    println("The answer to the question ${q.Question} is ${q.Answer}")
-
-    val message = if(q.Answer == q.CorrectAnswer) "You were correct" else "Try again?"
-
-    println(message)
-
-    q.printResult()
-
-    val number:Int? = try {
-        Integer.parseInt(q.Answer)
-    } catch (e: NumberFormatException){
-        null
+    //for (i in 1 .. 10 step 2)
+    //for (i in 1 until  10)
+    for (i in numbers){
+        println(i)
     }
 
-    println("Number is $number")
+    var ages = TreeMap<String, Int>();
+    ages["Kevin"] = 55
+    ages["Sam"] = 24
+    ages["Alex"] = 24
+    ages["Harry"] = 26
+
+    for ((name, age) in ages){
+        println("$name is $age")
+    }
+
+    for((index, element) in numbers.withIndex()){
+        println("$element at $index")
+    }
+
+    var range = 'a'..'z'
+
+//    println("Hello, world")
+//    val q:Question = Question()
+//    //q.Answer  = "42"
+//    q.Answer  = "Four"
+//   //.Question = ""
+//
+//
+//    q.display()
+//
+//    println("The answer to the question ${q.Question} is ${q.Answer}")
+//
+//    val message = if(q.Answer == q.CorrectAnswer) "You were correct" else "Try again?"
+//
+//    println(message)
+//
+//    q.printResult()
+//
+//    val number:Int? = try {
+//        Integer.parseInt(q.Answer)
+//    } catch (e: NumberFormatException){
+//        null
+//    }
+//
+//    println("Number is $number")
 }
 
 class Question(){
