d2499d677c709acc22ca772ba0166de12c8baad4
Jamesson Faria <jamessonjr@gmail.com>
2022-07-15 14:14:01 -0300

    Using companion objects

--- src/main/kotlin/br/com/jamesson/PersonConstructor.kt
+++ src/main/kotlin/br/com/jamesson/PersonConstructor.kt
@@ -9,7 +9,12 @@ fun main() {
 
     Courses.initialize()
     val curso = carlos.enrole("Kotlin Avancado")
-    print(curso)
+    println(curso)
+
+    Student3.createPostgrad("Simon")
+    Student3.createUndergrad("Kevin")
+
+    println(Student3.toXml(carlos))
 }
 
 abstract class PersonConstructor(var firstName: String, var lastName: String) {
@@ -19,7 +24,7 @@ abstract class PersonConstructor(var firstName: String, var lastName: String) {
 }
 
 //class Student3(val id: Int) : PersonConstructor() {
-class Student3(firstName: String, lastName: String, _id: Int, var tutor: String = "") : PersonConstructor(firstName, lastName) {
+open class Student3(firstName: String, lastName: String, _id: Int, var tutor: String = "") : PersonConstructor(firstName, lastName) {
     //val id = _id
     val id: Int
     //var tutor: String
@@ -48,4 +53,31 @@ class Student3(firstName: String, lastName: String, _id: Int, var tutor: String 
     override fun getName(): String {
         return ""
     }
+
+    // Companion Object = permite a classe estudante virar uma fabrica
+    // das classes concretas que as herdam
+    // tambem é permitido um companion object implementar uma interface
+    companion object : XmlSerializer<Student3> {
+        fun createUndergrad(name: String) : Undergraduate {
+            return Undergraduate(name)
+        }
+
+        fun createPostgrad(name: String) : Postgraduate {
+            return Postgraduate(name)
+        }
+
+        override fun toXml(Item: Student3) : String {
+            return "<xml>${Item.id} - ${Item.firstName}</xml>"
+        }
+    }
+}
+
+class Undergraduate(firstName: String) : Student3(firstName, "", 1){
+}
+
+class Postgraduate(firstName: String) : Student3(firstName, "", 1){
+}
+
+interface XmlSerializer<T> {
+    fun toXml(Item: T) : String
 }
