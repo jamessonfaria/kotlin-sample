e8ecddce9f5a012b3fba041f872612cc7632ddfb
Jamesson Junior <jamessonjr@gmail.com>
2022-07-25 15:54:31 -0300

    Using SAM Constructors

--- src/main/kotlin/br/com/jamesson/interface_funcional/kotlin/Program.kt
+++ src/main/kotlin/br/com/jamesson/interface_funcional/kotlin/Program.kt
@@ -1,12 +1,18 @@
 package br.com.jamesson.interface_funcional.kotlin
 
+import br.com.jamesson.interface_funcional.java.Created
 import br.com.jamesson.interface_funcional.java.User
-
 fun main() {
     var user = User("Kevin")
 
     var count = 0
 
-    user.create { println("User $it has been created: ${++count} times") }
+    //var eventListener = { u: User -> println("User $u has been created: ${++count} times") }
+    // USANDO CONSTRUTOR SAM PARA FUNCOES ANONIMAS, CRIADO PELO COMPILADOR
+    var eventListener = Created { println("User $it has been created: ${++count} times") }
+
+    user.create(eventListener)
+
+
     user.create { println("User $it has been created: ${++count} times") }
 }
